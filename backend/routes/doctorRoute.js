3944bc00abca086767583f2ecdcbbfb51f7ca8a7
S.Agalya <agalyaishere@gmail.com>
2024-10-09 00:18:15 +0530

    commit

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginDoctor, appointmentsDoctor, appointmentCancel,appointmentComplete} from "../controllers/doctorController.js";
+import { loginDoctor, appointmentsDoctor, appointmentCancel,appointmentComplete,doctorDashboard} from "../controllers/doctorController.js";
 import authDoctor from "../middlewares/authDoctor.js";
 
 const doctorRouter=express.Router()
@@ -8,5 +8,7 @@ doctorRouter.post('/login',loginDoctor)
 doctorRouter.get('/appointments',authDoctor,appointmentsDoctor)
 doctorRouter.post('/complete-appointment',authDoctor, appointmentComplete)
 doctorRouter.post('/cancel-appointment',authDoctor, appointmentCancel)
+doctorRouter.get('/dashboard',authDoctor,doctorDashboard)
+
 
 export default doctorRouter
